ba0983a89a4d671444d317f7e1487b9c296fbf12
pingulee <pigulee@soosan.co.kr>
2024-09-09 22:30:32 +0900

    backup

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <windows.h>
 
 /*
  * 문자열 길이 구하는 함수 (like strlen)
@@ -22,8 +24,8 @@ int my_strlen(const char *str) {
 @param 
 */
 int my_strcmp(const char *str1, const char *str2) {
-    int len1 = string_length(str1);
-    int len2 = string_length(str2);
+    int len1 = my_strlen(str1);
+    int len2 = my_strlen(str2);
 
         if (len1 != len2) {
         return 0;
@@ -38,7 +40,54 @@ int my_strcmp(const char *str1, const char *str2) {
     return 1;
 }
 
+/*
+ * UTF-8 바이트 배열을 디코딩하여 문자열로 변환하는 함수
+ * @param utf8_bytes 입력된 UTF-8 바이트 배열
+ * @param output 디코딩된 문자열을 저장할 버퍼
+ */
+// void utf8_decode(const uint8_t *utf8_bytes, char *output) {
+//     int i = 0;
+//     int j = 0;
+
+//     while (utf8_bytes[i] != 0) {
+//         uint8_t byte = utf8_bytes[i];
+        
+//         // 1바이트 문자 (ASCII)
+//         if ((byte & 0x80) == 0) {
+//             output[j++] = byte;
+//         }
+//         // 2바이트 문자
+//         else if ((byte & 0xE0) == 0xC0) {
+//             uint16_t code_point = ((byte & 0x1F) << 6) | (utf8_bytes[i+1] & 0x3F);
+//             output[j++] = (char)code_point;
+//             i++; // Skip the next byte
+//         }
+//         // 3바이트 문자
+//         else if ((byte & 0xF0) == 0xE0) {
+//             uint16_t code_point = ((byte & 0x0F) << 12) | ((utf8_bytes[i+1] & 0x3F) << 6) | (utf8_bytes[i+2] & 0x3F);
+//             output[j++] = (char)code_point;
+//             i += 2; // Skip the next 2 bytes
+//         }
+//         // 4바이트 문자
+//         else if ((byte & 0xF8) == 0xF0) {
+//             uint32_t code_point = ((byte & 0x07) << 18) | ((utf8_bytes[i+1] & 0x3F) << 12) | ((utf8_bytes[i+2] & 0x3F) << 6) | (utf8_bytes[i+3] & 0x3F);
+//             // Note: Code points for characters outside the BMP (Basic Multilingual Plane) need additional handling
+//             output[j++] = (char)code_point; // Simplification for this example
+//             i += 3; // Skip the next 3 bytes
+//         }
+        
+//         i++;
+//     }
+//     output[j] = '\0';
+// }
+
 int main() {
-    my_strcmp;
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    char output[256];
+
+    scanf("%s", output);
+    printf("%s\n", output);
+    getchar();
     return 0;
 }
