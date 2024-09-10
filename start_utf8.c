a94cdb32d01573c3166bfa43c341bc3304366746
이민구 <escapeweedy@gmail.com>
2024-09-11 08:01:19 +0900

    오류가 났을 때 뒤 숫자 표시 안됨

--- start_utf8.c
+++ start_utf8.c
@@ -1,33 +1,91 @@
 #include <stdio.h>
 
-int utf8_decode(const char* hexStr, int *char_count) {
+int valid(const char* hex_code, int* byte_good) {
     unsigned int hex;
-    int byte_count = 0;
-    int count = 0;
+    int byte_check = 0;
 
-    while (*hexStr) {
-        if (sscanf(hexStr, "%2x", &hex) != 1) break;
-        if ((hex & 0xC0) != 0x80) {
-            count++;
+    *byte_good = 0;
+
+    while (*hex_code) {
+        if (sscanf(hex_code, "%2x", &hex) != 1) break;
+
+        // 첫비트 확인
+        if (byte_check == 0) { 
+            if ((hex & 0x80) == 0x00) { // 0xxxxxxx = 1바이트
+                byte_check = 0;
+            } else if ((hex & 0xE0) == 0xC0) { // 110xxxxx = 2바이트
+                byte_check = 1;
+            } else if ((hex & 0xF0) == 0xE0) { // 1110xxxx = 3바이트
+                byte_check = 2;
+            } else if ((hex & 0xF8) == 0xF0) { // 11110xxx = 4바이트
+                byte_check = 3;
+            } else {
+                return 0;
+            }
+        } else {
+            if ((hex & 0xC0) != 0x80) {
+                return 0;
+            }
+            byte_check--;
         }
+
+        (*byte_good)++;
+        while (*hex_code && *hex_code != ' ') hex_code++;
+        if (*hex_code == ' ') hex_code++;
+    }
+
+    return byte_check == 0;
+}
+
+void utf8_decode(const char* hex_code) {
+    unsigned int hex;
+    int char_count = 0;
+    int byte_count = 0;
+    int byte_good = 0;
+
+    if (!valid(hex_code, &byte_good)) {
+        while (byte_good > 0 && *hex_code) {
+            if (sscanf(hex_code, "%2x", &hex) != 1) break;
+            printf("%c", hex);
+            byte_count++;
+
+            if ((hex & 0xC0) != 0x80) { 
+                char_count++;
+            }
+
+            while (*hex_code && *hex_code != ' ') hex_code++;
+            if (*hex_code == ' ') hex_code++;
+
+            byte_good--;
+        }
+        printf("\nerror!\n");
+        return;
+    }
+
+    while (*hex_code) {
+        if (sscanf(hex_code, "%2x", &hex) != 1) break;
         printf("%c", hex);
         byte_count++;
-        while (*hexStr && *hexStr != ' ') hexStr++;
-        if (*hexStr == ' ') hexStr++;
+
+        if ((hex & 0xC0) != 0x80) { 
+            char_count++;
+        }
+
+        while (*hex_code && *hex_code != ' ') hex_code++;
+        if (*hex_code == ' ') hex_code++;
     }
-    *char_count = count;
-    return byte_count;
+
+    printf(" %d %d\n", char_count, byte_count);
 }
 
 int main() {
-    char input[256] = {};
+    char input[256];
+
     while (1) {
         printf("> ");
         fgets(input, sizeof(input), stdin);
 
-        int char_count;
-        int byte_count = utf8_decode(input, &char_count);
-        printf(" %d %d\n", char_count, byte_count);
+        utf8_decode(input);
     }
 
     return 0;
