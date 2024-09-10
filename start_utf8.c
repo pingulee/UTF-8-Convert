13196c2c7074f6fe39bbbaef40e5d0ee9ef23d8f
이민구 <escapeweedy@gmail.com>
2024-09-11 04:34:59 +0900

    바이트 수 표현 완료

--- start_utf8.c
+++ start_utf8.c
@@ -1,50 +1,28 @@
 #include <stdio.h>
 
-/* 문자열 길이 구하는 함수 (like strlen)
- * @param str 길이를 구할 문자열
- * @return 문자열의 길이
- */
-int my_strlen(const char *str) {
-    int length = 0;
-    while (str[length] != '\0') {
-        length++;
+void utf8_decode(const char* hexStr) {
+    unsigned int hex;
+    int char_count = 0;
+    int byte_count = 0;
+
+    while (*hexStr) {
+        if (sscanf(hexStr, "%2x", &hex) != 1) break;
+        printf("%c", hex);
+        byte_count++;
+        while (*hexStr && *hexStr != ' ') hexStr++;
+        if (*hexStr == ' ') hexStr++;
     }
-    return length;
-}
-
-void hex_to_utf8(const char *hex) {
-    unsigned int len = my_strlen(hex);
-    unsigned char byte;
-    unsigned char buffer[256];
-    unsigned int buffer_index = 0;
-
-    for (unsigned int i = 0; i < len; i += 3) {
-        if (sscanf(hex + i, "%2hhx", &byte) != 1) {
-            fprintf(stderr, "잘못된 16진수 형식입니다.\n");
-            return;
-        }
-        buffer[buffer_index++] = byte;
-    }
-    buffer[buffer_index] = '\0';
-
-    printf("UTF-8 문자열: %s\n", buffer);
+    printf(" %d\n", byte_count);
 }
 
 int main() {
+    char input[256] = {};
+
     while (1) {
-        char hex_input[512];
         printf("> ");
-        if (fgets(hex_input, sizeof(hex_input), stdin) == NULL) {
-            perror("입력 오류");
-            return 1;
-        }
+        fgets(input, sizeof(input), stdin);
 
-        unsigned int len = my_strlen(hex_input);
-        if (len > 0 && hex_input[len - 1] == '\n') {
-            hex_input[len - 1] = '\0';
-        }
-
-        hex_to_utf8(hex_input);
+        utf8_decode(input);
     }
 
     return 0;
