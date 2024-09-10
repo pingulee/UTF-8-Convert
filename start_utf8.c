d43ce5f5834e325a34ab4b94888decc2b466873b
이민구 <escapeweedy@gmail.com>
2024-09-11 05:04:58 +0900

    글자 갯수 완료

--- start_utf8.c
+++ start_utf8.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
 
-void utf8_decode(const char* hexStr) {
+int utf8_decode(const char* hexStr, int *char_count) {
     unsigned int hex;
-    int char_count = 0;
     int byte_count = 0;
+    int count = 0;
 
     while (*hexStr) {
         if (sscanf(hexStr, "%2x", &hex) != 1) break;
+        if ((hex & 0xC0) != 0x80) {
+            count++;
+        }
         printf("%c", hex);
         byte_count++;
         while (*hexStr && *hexStr != ' ') hexStr++;
         if (*hexStr == ' ') hexStr++;
     }
-    printf(" %d\n", byte_count);
+    *char_count = count;
+    return byte_count;
 }
 
 int main() {
     char input[256] = {};
-
     while (1) {
         printf("> ");
         fgets(input, sizeof(input), stdin);
 
-        utf8_decode(input);
+        int char_count;
+        int byte_count = utf8_decode(input, &char_count);
+        printf(" %d %d\n", char_count, byte_count);
     }
 
     return 0;
