f4d8a84c58f551986466df55caf46838d187ae5e
이민구 <escapeweedy@gmail.com>
2024-09-11 09:21:49 +0900

    오류 검증

--- start_utf8.c
+++ start_utf8.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
-int valid(const char* hex_code, int* byte_good, int* err_num) {
+int valid(const char* hex_code, int* byte_good, int* err_char_num, int* err_byte_num) {
     unsigned int hex;
     int byte_check = 0;
     int char_num = 0;
+    int byte_num = 0;
 
     *byte_good = 0;
-    *err_num = 0;
+    *err_char_num = 0;
+    *err_byte_num = 0;
 
     while (*hex_code) {
         if (sscanf(hex_code, "%2x", &hex) != 1) break;
 
-        // 첫비트 확인
         if (byte_check == 0) { 
             if ((hex & 0x80) == 0x00) { // 0xxxxxxx = 1바이트
                 byte_check = 0;
@@ -22,18 +23,21 @@ int valid(const char* hex_code, int* byte_good, int* err_num) {
             } else if ((hex & 0xF8) == 0xF0) { // 11110xxx = 4바이트
                 byte_check = 3;
             } else {
-                *err_num = char_num + 1;
+                *err_char_num = char_num + 1;
+                *err_byte_num = byte_num + 1;
                 return 0;
             }
         } else {
             if ((hex & 0xC0) != 0x80) {
-                *err_num = char_num + 1;
+                *err_char_num = char_num + 1;
+                *err_byte_num = byte_num + 1;
                 return 0;
             }
             byte_check--;
         }
 
         (*byte_good)++;
+        byte_num++;
         while (*hex_code && *hex_code != ' ') hex_code++;
         if (*hex_code == ' ') hex_code++;
         
@@ -50,9 +54,10 @@ void utf8_decode(const char* hex_code) {
     int char_count = 0;
     int byte_count = 0;
     int byte_good = 0;
-    int err_num = 0;
+    int err_char_num = 0;
+    int err_byte_num = 0;
 
-    if (!valid(hex_code, &byte_good, &err_num)) {
+    if (!valid(hex_code, &byte_good, &err_char_num, &err_byte_num)) {
         while (byte_good > 0 && *hex_code) {
             if (sscanf(hex_code, "%2x", &hex) != 1) break;
             printf("%c", hex);
@@ -67,7 +72,7 @@ void utf8_decode(const char* hex_code) {
 
             byte_good--;
         }
-        printf(" 오류! %d번째 문자\n", err_num);
+        printf(" 오류! %d번째 문자, %d번째 바이트\n", err_char_num, err_byte_num);
         return;
     }
 
